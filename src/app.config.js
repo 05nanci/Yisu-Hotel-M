19cb8ea4affe4165f26caa8d8398c090526e906a
yyf2185 <2923263991@qq.com>
2026-02-07 10:14:42 +0800

    feat: initial project setup

---
+++ src/app.config.js
@@ -0,0 +1,14 @@
+export default defineAppConfig({
+  pages: [
+  'pages/hotel-detail/index',// 酒店详情页（注意路径和你的详情页一致）
+  'pages/index/index'        // 首页（可选）
+     // 酒店列表页
+      
+],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'WeChat',
+    navigationBarTextStyle: 'black'
+  }
+})
